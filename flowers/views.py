afadec96633fb2e75c90471f85882732b4842828
George <gnjuki19@gmail.com>
2023-04-24 14:16:05 +0300

    Added view level permissions for only admin is able to view the details page of each post. Users that aren't admin can only view the list and not the deatils of each post

--- flowers/views.py
+++ flowers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+# from rest_framework import generics
+from rest_framework import generics, permissions  # new
 
 from .models import Flowers
 from .serializers import FlowersSerializer
@@ -8,8 +9,9 @@ from .serializers import FlowersSerializer
 class FlowersList(generics.ListCreateAPIView):
     queryset = Flowers.objects.all()
     serializer_class = FlowersSerializer
-    
+
 class FlowersDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAdminUser,) # new
     queryset = Flowers.objects.all()
     serializer_class = FlowersSerializer
 
