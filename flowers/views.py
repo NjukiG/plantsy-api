2a6c8c592fa3d54f8ae7f0de9aab281fccfdcce5
George <gnjuki19@gmail.com>
2023-04-24 14:31:29 +0300

    Added permissions for authenticated users

--- flowers/views.py
+++ flowers/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 
-# from rest_framework import generics
-from rest_framework import generics, permissions  # new
+from rest_framework import generics
+# from rest_framework import generics, permissions  # new
 
 from .models import Flowers
+from .permissions import IsAuthorOrReadOnly  # new
 from .serializers import FlowersSerializer
 
 class FlowersList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)  # new
     queryset = Flowers.objects.all()
     serializer_class = FlowersSerializer
 
 class FlowersDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,) # new
     # permission_classes = (permissions.IsAdminUser,) # new
     queryset = Flowers.objects.all()
     serializer_class = FlowersSerializer
